8097f366f1a057729f1eb97e3ad40859751fde7d
JeremyAlejandro23 <141846943+JeremyAlejandro23@users.noreply.github.com>
2023-08-16 10:18:26 -0300

    Add files via upload

---
+++ ejer4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdLib.h>
+
+void P(float F,float A);
+
+
+int main() 
+{
+	
+	float F=0,A=0;
+	printf("Ingrese el valor de la Fuerza: \n");
+	scanf("%f",&F);
+	printf("Ingrese el valor del Area : \n");
+	scanf("%f",&A);
+	
+	P(F,A);
+	
+	system("pause");
+	return 0;	
+}
+void P(float F,float A)
+{
+	float Pre = 0;       
+	Pre = F/A ;
+	printf("Presion es de: %.2f\n\n",Pre);
+	
+}
+
+
